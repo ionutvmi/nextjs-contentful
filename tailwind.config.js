b0487abc7153dff8078af1bd69d90141550a1e38
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-05-22 21:33:52 +0300

    Adds the layouts folder to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     content: [
+        "./src/layouts/**/*.{js,ts,jsx,tsx}",
         "./src/pages/**/*.{js,ts,jsx,tsx}",
         "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
